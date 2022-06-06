fc4f07e1439e08912f1cf456723ba27a96cbb361
Maria210194 <mary.stachie@gmail.com>
2022-06-06 09:25:34 +0200

    add seeder for AddCategories

---
+++ database/seeds/AddCategoriesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+use Illuminate\Support\Str;
+
+class AddCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories=['Vegan', 'Fitness'];
+
+        foreach($categories as $category){
+            $new_category_object = new Category();
+            $new_category_object->name = $category;
+            $new_category_object->slug = Str::slug($category);
+            $new_category_object->save();
+        }
+    }
+}
